82b3898911e1eb2c0c64a21b978eff47aa769092
Kristian Wrona <kwrona@smail.uni-koeln.de>
2020-11-05 14:34:42 +0100

    Update HalloWorld.java

--- HalloWorld.java
+++ HalloWorld.java
@@ -3,6 +3,7 @@ public class HalloWorld {
 	public static void main(String[] args) {
         System.out.println("Kristian Wrona");
         System.out.println("TEST TEST");
+        System.out.println("new information");
 	}
 
 }
